58b8be8a766a5ac2c8c64f82dc9f51f56ccf0b1c
Paulius Savickas <pauliussavickas18@gmail.com>
2023-02-28 20:16:40 +0200

    event and user models added

---
+++ models/eventModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Please add a title']
+        },
+        category: {
+            type: String,
+            required: [true, 'Please add a category']
+        },
+        time: {
+            type: String, 
+            required: [true, 'Please add a time for the event']
+        },
+        price: {
+            type: String, 
+            required: [true, 'Please add a price Eur']
+        },
+        author: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+    },
+    {
+        timestamps: true
+   }
+)
+
+module.exports = mongoose.model('Event', eventSchema)
